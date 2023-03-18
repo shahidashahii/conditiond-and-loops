ea85aae061adbfe08196da1917c8140110738c10
shahidashahii <shahidashahi2000@gmail.com>
2023-03-18 14:04:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def conditions(request):
+    d={'a':1909,'b':2090}
+    return render(request,'conditions.html',context=d) 
+
+
+def loop(request):
+    d={'name':'ASHU'}
+    return render(request,'loop.html',d)
